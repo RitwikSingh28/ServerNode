f65f35a3a970525062eafc3d2c916fa5e7f13ccf
RitwikSingh28 <itsritwik26@gmail.com>
2022-12-18 13:22:22 +0530

    middleware complete

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const app = express();
 
+const cors = require('cors');
 const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
 const path = require('path');
 const PORT = process.env.PORT || 3500;
 
 //custom middleware logger to continuously log the requests
 //refactored 
 app.use(logger);
+
+//Cross-Origin resource sharing for allowing access from domains
+//Allowing only select few domains to access the server 
+//use of third-party middleware
+const whitelist = ['https://www.someSampleWebsite.com', 'http://localhost:3500', 'http://127.0.0.1:5500'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if(whitelist.indexOf(origin) != -1 || !origin){
+            callback(null, true);
+        }else{
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 
 //using built-in middleware for delivering static content
 app.use(express.static(path.join(__dirname, 'public')));
@@ -24,7 +42,20 @@ app.get('/old-page(.html)?', (req, res) => {
     res.redirect(301, '/new-page.html'); //302 is the default 
 });
 
-app.get('/*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+//using app.all() to handle all other events
+app.all('/*', (req, res) => {
+    res.status(404);
+    
+    if(req.accepts('html')){
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if(req.accepts('json')) {
+        res.json({error: "404 Not Found"});
+    } else {
+        res.type('text').send('404 Not Found');
+    }
 });
+
+//middleware for handling errors
+app.use(errorHandler);
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
