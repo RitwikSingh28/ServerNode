c93fbfb2a17bbd8890cc5083ab6c8b2315191a84
RitwikSingh28 <itsritwik26@gmail.com>
2022-12-18 15:45:56 +0530

    basic routing refactoring

--- server.js
+++ server.js
@@ -29,18 +29,10 @@ app.use(cors(corsOptions));
 
 //using built-in middleware for delivering static content
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/subdir' ,express.static(path.join(__dirname, 'public')));
 
-app.get('^/$|/index(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
-
-app.get('/new-page(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
-});
-
-app.get('/old-page(.html)?', (req, res) => {
-    res.redirect(301, '/new-page.html'); //302 is the default 
-});
+app.use('/', require('./routes/root'));
+app.use('/subdir', require('./routes/subdir')); 
 
 //using app.all() to handle all other events
 app.all('/*', (req, res) => {
